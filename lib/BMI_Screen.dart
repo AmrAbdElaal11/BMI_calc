80d3d1b1d2b60dbe5ee49774a1b2ad65ad0253b6
amr abd elaaal <amrmohamed362363@gmail.com>
2022-02-25 16:44:16 +0200

    starting bmi

---
+++ lib/BMI_Screen.dart
@@ -0,0 +1,13 @@
+class BMIScreen extends StatefulWidget {
+  const BMIScreen({Key? key}) : super(key: key);
+
+  @override
+  _BMIScreenState createState() => _BMIScreenState();
+}
+
+class _BMIScreenState extends State<BMIScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
